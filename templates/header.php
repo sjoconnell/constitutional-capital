9e1edaed877576634a80bfebe3592b5007ecf6d9
Sam O'Connell <sjo773@gmail.com>
2015-12-21 17:10:54 -0600

    header almost completed, need to make items line up closer

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,20 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="http://firstsage.dev/">
+            <img class="cc-logo" src="http://firstsage.dev/wp-content/uploads/2015/12/Bitmap-Copy.png">
+          </a>
+        </div>
+
+        <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          endif;
+          ?>
+
+        <a class="btn btn-default btn-lg navbar-right" href="http://firstsage.dev/contact" role="button">Contact Us</a>
+
+      </div>
     </nav>
-  </div>
 </header>
