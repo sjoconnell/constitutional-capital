ab8ca18329587e4b66b5a713ec401762529f19e0
Sam O'Connell <sjo773@gmail.com>
2015-12-21 22:41:07 -0600

    started on footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,14 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  <div class="container-footer">
+    <div class="address-footer">
+        <img src="http://firstsage.dev/wp-content/uploads/2015/12/Bitmap-Copy.png">
+        <p>141 W. Jackson Blvd. Chicago, IL 60604</p>
+        <p>312.282.1203</p>
+    </div>
+
+    <div class="form-footer">
+        
+    </div>
+
   </div>
 </footer>
--- templates/front-page.php
+++ templates/front-page.php
@@ -1,4 +1,4 @@
 <?php /* Template Name: Home */ ?>
 <?php while (have_posts()) : the_post(); ?>
-
+    <h1>Homepage Body</h1>
 <?php endwhile; ?>
