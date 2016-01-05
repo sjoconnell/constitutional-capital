3d964825a093d9d6af44639619409d9f61a4571a
Sam O'Connell <sjo773@gmail.com>
2016-01-05 17:01:17 -0600

    added custom field for homepage banner

--- templates/front-page.php
+++ templates/front-page.php
@@ -4,8 +4,8 @@
         <div class="home-image-container">
             <img class="home-background" src="http://firstsage.dev/wp-content/uploads/2015/12/Bitmap3.png">
             <div class="home-background-text">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                <a class="btn btn-default btn-lg" href="#" role="button">SERVICES</a>
+                <p><?php the_field('homepage-banner-text');?></p>
+                <a class="btn btn-default btn-lg" href="<?php the_field('homepage-banner-button-link');?>" role="button"><?php the_field('homepage-banner-button-text');?></a>
             </div>
         </div>
 
