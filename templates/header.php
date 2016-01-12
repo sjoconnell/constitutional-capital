327ddb0e5e5e94145155beb05accb9605100b420
Sam O'Connell <sjo773@gmail.com>
2016-01-12 13:56:13 -0600

    added contact us to list menu in nav

--- templates/header.php
+++ templates/header.php
@@ -5,15 +5,24 @@
           <a class="navbar-brand" href="<?= the_field('header-logo-link', 6);?>">
             <img class="cc-logo" src="<?= the_field('header-logo', 6);?>">
           </a>
+
+          <button class="navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
         </div>
 
+        <div class="collapse navbar-collapse navHeaderCollapse">
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
           endif;
           ?>
+        </div>  
 
-        <a class="btn btn-default btn-lg navbar-right" href="<?= the_field('header-button-link', 6);?>" role="button"><?php the_field('header-button-text', 6);?></a>
+<!--         <a class="btn btn-default btn-lg navbar-right" href="<?= the_field('header-button-link', 6);?>" role="button"><?php the_field('header-button-text', 6);?></a> -->
 
       </div>
     </nav>
