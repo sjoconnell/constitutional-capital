adb5c4de42a17b3818baf8a93a7782edfbed3157
Sam O'Connell <sjo773@gmail.com>
2016-01-13 17:54:04 -0600

    added mobile nav menu except for jquery

--- base.php
+++ base.php
@@ -8,23 +8,35 @@ use Roots\Sage\Wrapper;
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <body <?php body_class(); ?>>
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
+        <div class="mobile-header-menu">
+          <div class="mobile-header-menu-toprow">
+            <i class="fa fa-times fa-4x"></i>
+          </div>
+          <?php
+            if (has_nav_menu('mobile_navigation')) :
+              wp_nav_menu(['theme_location' => 'mobile_navigation', 'menu_class' => 'mobile-nav']);
+            endif;
+            ?>
+        </div>
     <?php
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-<!--     <div class="wrap container" role="document">
+<!-- <div class="wrap container" role="document">
       <div class="content row">
         <main class="main"> -->
           <?php include Wrapper\template_path(); ?>
         <!-- </main>/.main -->
       <!-- </div>/.content -->
     <!-- </div>/.wrap -->
+
     <?php
       do_action('get_footer');
       get_template_part('templates/footer');
--- templates/header.php
+++ templates/header.php
@@ -6,23 +6,25 @@
             <img class="cc-logo" src="<?= the_field('header-logo', 6);?>">
           </a>
 
-          <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+          <div class="hamburger-menu">
+            <i class="fa fa-bars fa-3x"></i>
+          </div>
+
 
         </div>
 
-        <div class="collapse navbar-collapse navHeaderCollapse">
+        <div class="main-header-nav">
           <?php
             if (has_nav_menu('primary_navigation')) :
               wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
             endif;
             ?>
+        </div>
 
+        <div class="main-header-nav-button">
           <a class="btn btn-default btn-lg navbar-right" href="<?= the_field('header-button-link', 6);?>" role="button"><?php the_field('header-button-text', 6);?></a>
         </div>
+
       </div>
     </nav>
 </header>
